040f67d60ac544be37375dfa37c163008fe8e40f
James Shie <jamesshie@supermicro.com>
2016-02-03 18:12:55 +0800

    C: create first www folder.

---
+++ MPMS/www/xmlhttpreq.js
@@ -0,0 +1,90 @@
+/*
+ *http://blog.chinaunix.net/uid-28264320-id-4013835.html
+ *create items
+ */
+function createXHR() 
+{
+    var xhr;
+
+
+    try 
+    {
+        xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    } 
+    catch (e) 
+    {
+        try 
+        {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch(E) 
+        {
+            xhr = false;
+        }
+    }
+
+    if (!xhr && typeof XMLHttpRequest != 'undefined') 
+    {
+        xhr = new XMLHttpRequest();
+    }
+
+    return xhr;
+}
+
+/*
+ *
+ */
+function sender() 
+{
+    xhr = createXHR();
+
+    if(xhr)
+    {
+        xhr.onreadystatechange=callbackFunction;
+    
+
+        //test.cgi
+        xhr.open("GET", "/cgi-bin/test.cgi?cur_time=" + new Date().getTime());
+    
+        xhr.send(null);
+    }
+    else
+    {
+        //XMLHttpRequest
+        alert("Broswer not support!");
+    }
+}
+
+function updateTimer()
+{
+    sender();
+    setTimeout("updateTimer()", 1000);
+}
+
+/*
+ *Handle function
+ */
+function callbackFunction()
+{
+    if (xhr.readyState == 4) 
+    {
+        if (xhr.status == 200) 
+        {
+            var returnValue = xhr.responseText;
+
+            if(returnValue != null && returnValue.length > 0)
+            {
+                document.getElementById("current_time").innerHTML = returnValue;
+            }
+            else
+            {
+                alert("Result is invail");
+            }
+        } 
+        else 
+        {
+            alert("Page is not right!");
+        }
+    }
+}
+
